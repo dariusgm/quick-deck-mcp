ed4b7d33c869f49f29c9476a593621e988584f52
Darius Murawski <dariusmurawski@hotmail.com>
2025-07-29 22:36:34 +0200

    Enhance logging and update prompt functions for agenda and content generation

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from string import Template
 from typing import List, Literal
 from mcp.server.fastmcp import FastMCP, Context
@@ -6,6 +7,12 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from openai.types.chat import ChatCompletionMessage
 from pydantic import BaseModel
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(sys.stdout, serialize=True)  # JSON output
+logger.info("MCP Booting")
 
 def _client():
     load_dotenv()
@@ -18,9 +25,6 @@ class InputAgenda(BaseModel):
     audience: str
     style: str
     language: str
-
-class OutputAgenda(BaseModel):
-    agenda: List[str]
 
 class Slide(BaseModel):
     title: str
@@ -79,7 +83,7 @@ def send_message(message: list[dict[str, str | list[dict[str, str]]]]) -> ChatCo
 
 
 @mcp.prompt("Generate an agenda for a presentation")
-def generate_agenda_prompt(settings: InputAgenda) -> list[dict[str, str | list[dict[str, str]]]]:
+def generate_agenda_prompt(settings: dict) -> list[dict[str, str | list[dict[str, str]]]]:
     developer_template = Template("""
        I want to to act as an expert in presentations for $style. 
        You will receive a topic of my choice and provide slides and speaker notes for me.
@@ -106,12 +110,17 @@ def generate_agenda_prompt(settings: InputAgenda) -> list[dict[str, str | list[d
     The topic is about $topic and the audience is $audience. Use $language for the content.
     """)
 
-    developer = developer_template.substitute(style=settings.style)
-    user = user_template.substitute(topic=settings.topic, audience=settings.audience, language=settings.language)
+    developer = developer_template.substitute(style=settings.get('style'))
+    user = user_template.substitute(
+        topic=settings.get('topic'),
+        audience=settings.get('audience'),
+        language=settings.get('language')
+    )
+    logger.info("Agenda Prompt: {} {}", developer, user)
     return send_template(developer, user)
 
 @mcp.prompt("Generate Content for a presentation")
-def generate_content_prompt(settings: InputAgenda, agenda: OutputAgenda) -> list[
+def generate_content_prompt(settings: dict, agenda: str) -> list[
     dict[str, str | list[dict[str, str]]]]:
     developer_template = Template("""
       I want to to act as an expert in presentations for $style. 
@@ -138,25 +147,38 @@ def generate_content_prompt(settings: InputAgenda, agenda: OutputAgenda) -> list
     """)
 
 
-    developer = developer_template.substitute(style=settings.style)
-    user = user_template.substitute(topic=settings.topic, audience=settings.audience, language=settings.language, style=settings.style, header=agenda.title)
+    developer = developer_template.substitute(style=settings.get('style'))
+    user = user_template.substitute(
+        topic=settings.get('topic'),
+        audience=settings.get('audience'),
+        language=settings.get('language'),
+        style=settings.get('style'),
+        header=agenda)
+    logger.info("Content Prompt: {} {}", developer, user)
     return send_template(developer, user)
 
 
 # Basic prompt tools
 @mcp.tool("Agenda Generation")
-def generate_agenda(settings: InputAgenda) -> OutputAgenda:
+def generate_agenda(settings: dict) -> List[str]:
+    logger.info("Generating agenda with this settings: {} ", settings)
     agenda_prompt = generate_agenda_prompt(settings)
     response = send_message(agenda_prompt).content
-    result = OutputAgenda(agenda=response.split("---"))
-    return result
+    if response:
+        result = response.split("---")
+        return result
+    else:
+        return []
 
 @mcp.tool("Content Generation")
-def generate_content(settings: InputAgenda, agenda: List[OutputAgenda]) -> List[Slide]:
+def generate_content(settings: dict, agenda: List[str]) -> List[Slide]:
     result = []
     for agenda_element in agenda:
+        logger.info("Generating Content for agenda element: {} ", agenda_element)
         content_prompt = generate_content_prompt(settings, agenda_element)
         response = send_message(content_prompt).content
+        if not response:
+            continue
         for paragraph in response.split("---"):
             paragraph = paragraph.strip()
             if paragraph.strip() == "":
@@ -165,7 +187,7 @@ def generate_content(settings: InputAgenda, agenda: List[OutputAgenda]) -> List[
             if paragraph.startswith("-"):
                 paragraph = paragraph[1:]
 
-            slide = Slide(content=paragraph, title=agenda_element.title)
+            slide = Slide(content=paragraph, title=agenda_element)
             result.append(slide)
     return result
 
