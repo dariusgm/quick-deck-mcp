9008056f577d92ddcd8c59b0a5b56b0a71280864
Darius Murawski <darius@murawski.blog>
2025-08-11 22:48:34 +0200

    Add end-to-end tests for content generation and export functionality

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import time
 import requests
@@ -47,9 +48,46 @@ def test_full_stack_with_server():
 
     try:
         # Send a request on the /tools/call/generate_agenda endpoint
-        response = requests.post(f"http://127.0.0.1:{port}/tools/call/generate_agenda", json=body, headers=headers)
-        assert response.status_code == 200
-        assert "Git" in response.text
+        response_generate_agenda = requests.post(f"http://127.0.0.1:{port}/tools/call/generate_agenda", json=body, headers=headers)
+        assert response_generate_agenda.status_code == 200
+        assert "Git" in response_generate_agenda.text
+
+
+        response_generate_content = requests.post(f"http://127.0.0.1:{port}/tools/call/generate_content", json=body, headers=headers)
+        assert response_generate_content.status_code == 200
+        assert "Git" in response_generate_content.text
+        with open("tests/e2e.md", "wt") as file:
+            file.write(response_generate_content.text)
+        assert len(response_generate_content.text) > len(response_generate_agenda.text)
+
+        with open("tests/e2e.md", "rt") as file:
+            response_export = requests.post(
+                f"http://127.0.0.1:{port}/tools/call/export",
+                headers={"Accept": "text/html"},
+                files={"file": file}
+            )
+
+        assert response_export.status_code == 202
+        job_id = response_export.json().get("job_id")
+
+        # now we can pull regularly the export file
+        success = False
+        for _ in range(50):
+            if not success:
+                response_export_status = requests.get(f"http://127.0.0.1:{port}/tools/call/export_status?job_id={job_id}")
+                if response_export_status.status_code == 200:
+                    response_export_status_json = response_export_status.json()
+                    if response_export_status_json["status"] == "done":
+                        success = True
+                        break
+                    else:
+                        time.sleep(5)
+        if success:
+            response_export_file = requests.get(f"http://127.0.0.1:{port}/tools/call/export?job_id={job_id}")
+            with open(f"tests/e2e.pdf", "wb") as file:
+                file.write(response_export_file.content)
+        assert success == True
+        assert os.path.exists("tests/e2e.pdf")
     finally:
         # Stop the server
         server.terminate()
